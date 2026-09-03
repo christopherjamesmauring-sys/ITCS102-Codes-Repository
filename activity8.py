804ab414b5218f378adb251bb6d1a22225310fda
christopherjamesmauring-sys <christopherjamesmauring@gmail.com>
2026-09-03 12:32:32 +0800

    Add files via upload

---
+++ activity8.py
@@ -0,0 +1,15 @@
+hobbies = "" #blank string
+
+h = input("What are your hobbies --> ")
+hobbies += h + ", "
+
+h = input("What is your favorite color --> ")
+hobbies += h + ", "
+
+h = input("What are you doing today --> ")
+hobbies += h + ", "
+
+h = input("What's your gender --> ")
+hobbies += h + ", "
+print("My hobbies are", hobbies)
+
