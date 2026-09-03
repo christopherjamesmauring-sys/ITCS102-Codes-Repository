ebc6552840ee96ca4ba177ba0e81999f5a667da5
christopherjamesmauring-sys <christopherjamesmauring@gmail.com>
2026-09-03 12:33:30 +0800

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,15 @@
+#integer varible
+myAge = 18
+
+#float variavble
+myHeight = 148
+
+#string variable
+password = "kalbokiniskintab"
+
+#boolean variable
+isBoy = True
+
+print("Hi, my password is", password)
+dir
+
---
+++ activity7.py
@@ -0,0 +1,14 @@
+total = 0
+n1 = eval(input("Enter a number --> "))
+total *= n1
+
+n2 = eval(input("Enter a number --> "))
+total1 = total = n2
+
+n2 = eval(input("Enter a number --> "))
+total = total1 + n2
+
+n3 = eval(input("Enter a number --> "))
+total1 *= n3
+
+print("The sum is ", total)
